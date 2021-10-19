5a3e861a02032534cbdb2a887b2303cf5ead3659
Dmitry <ddmitiy@mail.ru>
2021-10-19 23:52:36 +0300

    "jobs with image resolution"

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
-from django.forms import ModelChoiceField
+from django.forms import ModelChoiceField, ModelForm, ValidationError
 from django import forms
-
 from .models import *
+from PIL import Image
+
+class NotebookAdminForm(ModelForm):
+    MIN_RESOLUTION = (400,400)
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['image'].help_text = 'Загружайте изображение с мин разрешением {}x{}'.format(*self.MIN_RESOLUTION)
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img= Image.open(image)
+        # print(img.width, img.height)
+        min_height,min_width = self.MIN_RESOLUTION
+        if img.height <min_height or img.width < min_width:
+            raise ValidationError ('Разрешение изображения меньше минимального')
+
+        return image
 
 #отображение в админке только ноутбуков при добавлении ноутбуков
 
 class NotebookAdmin(admin.ModelAdmin):
+    form = NotebookAdminForm
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
             return ModelChoiceField(Category.objects.filter(slug='notebook'))
