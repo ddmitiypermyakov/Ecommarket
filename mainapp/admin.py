f47026256fed0782e78262db2cb02828429faa60
Dmitry <ddmitiy@mail.ru>
2021-10-19 17:27:15 +0300

    'Correct admin.py'

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,28 +1,22 @@
 from django.contrib import admin
+from django.forms import ModelChoiceField
 from django import forms
 
 from .models import *
 
 #отображение в админке только ноутбуков при добавлении ноутбуков
-class NotebookCategoryChoiceField(forms.ModelChoiceField):
-    pass
-
 
 class NotebookAdmin(admin.ModelAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return NotebookCategoryChoiceField(Category.objects.filter(slug='notebook'))
+            return ModelChoiceField(Category.objects.filter(slug='notebook'))
         return super().formfield_for_foreignkey(self, db_field, request, **kwargs)
 #отображение в админке только телефонов при добавлении ноутбуков
-
-class SmartphoneCategoryChoiceField(forms.ModelChoiceField):
-    pass
-
 
 class SmartphoneAdmin(admin.ModelAdmin):
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return NotebookCategoryChoiceField(Category.objects.filter(slug='smartphone'))
+            return ModelChoiceField(Category.objects.filter(slug='smartphone'))
         return super().formfield_for_foreignkey(self, db_field, request, **kwargs)
 
 admin.site.register(Category)
